a12a0242d49ad010c6ec023c8bd54aff8c71dd6d
kemblekaran <t-karan.kemble@ocltp.com>
2022-03-04 22:24:39 +0530

    day six, seven, eight

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -11,6 +11,21 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   var _userName = "";
   bool _pressed = false;
+  final _formKey = GlobalKey<FormState>();
+
+  void navigateToHomeScreen(BuildContext buildContext) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _pressed = true;
+      });
+
+      await Future.delayed(const Duration(milliseconds: 500));
+      await Navigator.pushNamed(context, CatalogueAppRoutes.homeRoute);
+      setState(() {
+        _pressed = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,64 +42,73 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Text(
             "Welcome $_userName",
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 20.0),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter you username", labelText: "User Name"),
-                    onChanged: (value) {
-                      _userName = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter your password", labelText: "Password"),
-                  ),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      setState(() {
-                        _pressed = true;
-                      });
-
-                      await Future.delayed(const Duration(seconds: 1));
-                      Navigator.pushNamed(
-                          context, CatalogueAppRoutes.homeRoute);
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      width: _pressed ? 75 : 150,
-                      height: 50,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          shape:
-                              _pressed ? BoxShape.circle : BoxShape.rectangle),
-                      child: _pressed
-                          ? const Icon(
-                              Icons.done,
-                              color: Colors.white,
-                            )
-                          : const Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter you username",
+                          labelText: "User Name"),
+                      onChanged: (value) {
+                        _userName = value;
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        return value != null && value.isEmpty
+                            ? "Username is mandatory"
+                            : null;
+                      },
                     ),
-                  )
-                ],
+                    TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            hintText: "Enter your password",
+                            labelText: "Password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password is mandatory";
+                          } else if (value.length > 6) {
+                            return "password must be 6 characters";
+                          }
+                          return null;
+                        }),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(_pressed ? 60 : 8),
+                      child: InkWell(
+                        onTap: () => navigateToHomeScreen(context),
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: _pressed ? 60 : 120,
+                          height: 60,
+                          alignment: Alignment.center,
+                          child: _pressed
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           )
